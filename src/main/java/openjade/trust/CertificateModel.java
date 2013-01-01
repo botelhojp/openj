2000152a3a9621f10d525773975905f42d0944dd
botelhojp@gmail.com <botelhojp@gmail.com>
2013-01-01 17:05:22 -0300

    CertificateModel

---
+++ src/main/java/openjade/trust/CertificateModel.java
@@ -0,0 +1,12 @@
+package openjade.trust;
+
+public class CertificateModel implements TrustModel {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String getName() {
+		return "CertificateModel";
+	}
+
+}

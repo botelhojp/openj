53e476885c672609e953f24cfc9a23a2dfc3bc47
botelhojp <botelhojp@gmail.com>
2013-06-05 22:58:00 -0300

    archetype ok!

--- archetype/open-jade/src/main/resources/archetype-resources/src/main/java/Boot.java
+++ archetype/open-jade/src/main/resources/archetype-resources/src/main/java/Boot.java
@@ -7,10 +7,6 @@ public class Boot {
 	protected static Logger log = Logger.getLogger(Boot.class);
 
 	public static void main(String[] args) {
-		if (args != null && args.length > 0) {
-			openjade.Boot.main(args);
-		} else {
-			openjade.Boot.loadXml();
-		}
+		openjade.Boot.loadXml();
 	}
 }
--- archetype/open-jade/src/main/resources/archetype-resources/src/main/java/Mygent.java
+++ archetype/open-jade/src/main/resources/archetype-resources/src/main/java/Mygent.java
@@ -1,15 +1,13 @@
 package ${packageName};
 
-import jade.core.behaviours.Behaviour;
-
-import java.io.InputStream;
-
+import jade.lang.acl.ACLMessage;
 import openjade.core.OpenAgent;
-import openjade.core.SignerAgent;
+import openjade.core.annotation.ReceiveSimpleMessage;
+import openjade.core.behaviours.SenderByServiceBehaviour;
 
 import org.apache.log4j.Logger;
 
-public class Mygent extends OpenAgent implements SignerAgent {
+public class Mygent extends OpenAgent {
 
 	private static final long serialVersionUID = 1L;
 
@@ -19,27 +17,46 @@ public class Mygent extends OpenAgent implements SignerAgent {
 		log.debug("setup: " + getAID().getLocalName());
 		registerService(getAID().getLocalName());
 		this.moveContainer("Hi-Container");
-		addBehaviour(new MyBehaviour(this, MyBehaviour.SIGNED));
-	}
-
-	public InputStream getKeystore() {
-		return Mygent.class.getResourceAsStream("/certs/" + getAID().getLocalName() + ".pfx");
-	}
-
-	public String getKeystorePassword() {
-		return "123456";
-	}
-
-	@Override
-	public void removeBehaviour(Behaviour _behaviour) {
-		super.removeBehaviour(_behaviour);
-		if (_behaviour instanceof MyBehaviour) {
-			MyBehaviour behaviour = (MyBehaviour) _behaviour;
-			if (behaviour.getMode().equals(MyBehaviour.SIGNED)) {
-				addBehaviour(new MyBehaviour(this, MyBehaviour.CIPHER_PRIVATE_KEY));
-			} else if (behaviour.getMode().equals(MyBehaviour.CIPHER_PRIVATE_KEY)) {
-				addBehaviour(new MyBehaviour(this, MyBehaviour.CIPHER_PUBLIC_KEY));
-			}
+		if (getAID().getLocalName().equals("agent2")) {
+			ACLMessage message = new ACLMessage(ACLMessage.PROPOSE);
+			message.setSender(getAID());
+			message.setConversationId("HI");
+			message.addReplyTo(getAID());
+			message.setContent("Hi agent1");
+			addBehaviour(new SenderByServiceBehaviour(this, "agent1", message, true));
 		}
+	}
+
+
+	@ReceiveSimpleMessage(conversationId = "HI", performative = { ACLMessage.PROPOSE })
+	public void receiveMessage(ACLMessage message) {
+		log.debug("received " + message.getSender().getLocalName() + ": \"" + message.getContent() + "\"");
+		ACLMessage reply = new ACLMessage(randomPerformative());
+		reply.addReceiver(message.getSender());
+		reply.setSender(getAID());
+		reply.setConversationId("HI");
+		reply.setContent(((reply.getPerformative() == ACLMessage.REFUSE) ? "Bye " : "Hi ") + message.getSender().getLocalName());
+		sendMessage(reply);
+	}
+
+	@ReceiveSimpleMessage(conversationId = "HI", performative = { ACLMessage.REFUSE })
+	public void receiveHiMessage(ACLMessage message) {
+		log.debug("received " + message.getSender().getLocalName() + ": \"" + message.getContent() + "\"");
+		ACLMessage reply = new ACLMessage(ACLMessage.REFUSE);
+		reply.addReceiver(message.getSender());
+		reply.setSender(getAID());
+		reply.setConversationId("BYE");
+		reply.setContent("Bye Bye " + message.getSender().getLocalName());
+		sendMessage(reply);
+	}
+
+	@ReceiveSimpleMessage(conversationId = "BYE", performative = { ACLMessage.REFUSE })
+	public void receiveByelMessage(ACLMessage message) {
+		log.debug("received " + message.getSender().getLocalName() + ": \"" + message.getContent() + "\"");
+	}
+
+	private int randomPerformative() {
+		int[] options = { ACLMessage.PROPOSE, ACLMessage.PROPOSE, ACLMessage.PROPOSE, ACLMessage.PROPOSE, ACLMessage.PROPOSE, ACLMessage.PROPOSE, ACLMessage.PROPOSE, ACLMessage.PROPOSE, ACLMessage.REFUSE };
+		return options[(int) (Math.random() * options.length)];
 	}
 }
